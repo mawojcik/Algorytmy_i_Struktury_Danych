9b873e7c8b70a1f802099983c86782c9d3c1161f
mawojcik <wojcik.maciej.k@gmail.com>
2023-05-27 13:18:45 +0200

    files to work with

---
+++ Semestr_II/graf/graf_spojnosc/GraphAsMatrix.h
@@ -0,0 +1,396 @@
+#include <vector>
+#include <iostream>
+#include "Vertex.h"
+#include "Edge.h"
+#include "Iterator.h"
+#include "CountingVisitor.h"
+
+class GraphAsMatrix
+{
+    std::vector<Vertex *> vertices;
+    std::vector<std::vector<Edge *> > adjacencyMatrix;
+    bool isDirected;
+    int numberOfVertices;
+    int numberOfEdges = 0;
+
+    class AllVerticesIter : public Iterator<Vertex>
+    {
+        GraphAsMatrix &owner;
+        int current;
+
+    public:
+        AllVerticesIter(GraphAsMatrix &owner) : owner(owner)
+        {
+            current = 0;
+            this->owner = owner;
+        };
+        bool IsDone()
+        {
+            if (current >= owner.vertices.size())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        Vertex &operator*() { return *owner.vertices[current]; }
+        void operator++() { current++; }
+    };
+
+    class AllEdgesIter : public Iterator<Edge>
+    {
+        GraphAsMatrix &owner;
+        int row;
+        int col;
+
+    public:
+        void next()
+        {
+            int width = owner.adjacencyMatrix.size();
+            if (!this->IsDone())
+            {
+                col++;
+                if (col >= width)
+                {
+                    row++;
+                    col = 0;
+                }
+            }
+            while (owner.adjacencyMatrix[col][row] == NULL && !this->IsDone())
+            {
+                col++;
+                if (col >= width)
+                {
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        AllEdgesIter(GraphAsMatrix &owner) : owner(owner)
+        {
+            this->owner = owner;
+            row = 0;
+            col = -1;
+        }
+        bool IsDone()
+        {
+            int width = owner.adjacencyMatrix.size();
+            if (col >= width - 1 && row >= owner.adjacencyMatrix[width - 1].size() - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        Edge &operator*()
+        {
+            if (col < 0)
+                next();
+            return *owner.adjacencyMatrix[col][row];
+        }
+        void operator++() { next(); }
+    };
+
+    class EmanEdgesIter : public Iterator<Edge>
+    {
+        GraphAsMatrix &owner;
+        int row;
+        int col;
+
+    public:
+        void next()
+        {
+            col++;
+            while (owner.adjacencyMatrix[col][row] == NULL && !this->IsDone())
+            {
+                col++;
+            }
+        }
+        EmanEdgesIter(GraphAsMatrix &owner, int v) : owner(owner)
+        {
+            row = v;
+            col = -1;
+            this->owner = owner;
+        }
+        bool IsDone()
+        {
+            int value = owner.adjacencyMatrix.size();
+            if (col >= (value - 1))
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        Edge &operator*()
+        {
+            if (col < 0)
+                next();
+            return *owner.adjacencyMatrix[col][row];
+        }
+        void operator++() { next(); }
+    };
+
+    class InciEdgesIter : public Iterator<Edge>
+    {
+        GraphAsMatrix &owner;
+        int row;
+        int col;
+
+    public:
+        void next()
+        {
+            row++;
+            while (owner.adjacencyMatrix[col][row] == NULL && !this->IsDone())
+            {
+                row++;
+            }
+        }
+        InciEdgesIter(GraphAsMatrix &owner, int v) : owner(owner)
+        {
+            row = -1;
+            col = v;
+            this->owner = owner;
+        }
+        bool IsDone()
+        {
+            int value = owner.adjacencyMatrix[0].size();
+            if (row >= (value - 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        Edge &operator*()
+        {
+            if (row < 0)
+                next();
+            return *owner.adjacencyMatrix[col][row];
+        }
+        void operator++() { next(); }
+    };
+
+public:
+    bool IsConnected()
+    {
+        if (!IsDirected())
+        {
+            Vertex *v = vertices[0];
+            CountingVisitor visitor;
+            std::vector<bool> visited(vertices.size(), false);
+            DFS_visitor(&visitor, v, visited);
+            std::cout << visitor.GetNumber() << std::endl;
+            std::cout << "visitor counter " << visitor.GetNumber()+1 << std::endl;
+            std::cout << "vertices size " << vertices.size() << std::endl;
+            return (visitor.GetNumber()+1 == vertices.size());
+        }
+        if (IsDirected())
+        {
+            Vertex *v;
+            int min = vertices.size();
+            for (int i = 0; i < vertices.size(); i++)
+            {
+                v = vertices[i];
+                CountingVisitor visitor;
+                std::vector<bool> visited(vertices.size(), false);
+                DFS_visitor(&visitor, v, visited);
+                if (visitor.GetNumber()+1 < min)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    bool IsStronglyConnected()
+    {
+        if (!IsDirected())
+        {
+            int arr[numberOfVertices];
+            for(int i = 0; i < numberOfVertices; i++) {
+                Vertex *v = vertices[i];
+                CountingVisitor visitor;
+                std::vector<bool> visited(vertices.size(), false);
+                DFS_visitor(&visitor, v, visited);
+//                std::cout << visitor.GetNumber() << std::endl;
+                arr[i] = visitor.GetNumber();
+                if ((i > 0) && (arr[i] != arr[i-1])) {
+                    return false;
+                }
+//                std::cout << "visitor counter " << visitor.GetNumber() << std::endl;
+//                std::cout << "vertices size " << vertices.size() << std::endl;
+            }
+
+//            return (visitor.GetNumber()+1 == vertices.size());
+            return true;
+        }
+        if (IsDirected())
+        {
+            Vertex *v;
+            int min = vertices.size();
+            for (int i = 0; i < vertices.size(); i++)
+            {
+                v = vertices[i];
+                CountingVisitor visitor;
+                std::vector<bool> visited(vertices.size(), false);
+                DFS_visitor(&visitor, v, visited);
+                if (visitor.GetNumber()+1 < min)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    void DFS(Vertex *v, bool StrongConnection)
+    {
+        CountingVisitor visitor;
+        std::vector<bool> visited(vertices.size(), false);
+        DFS_visitor(&visitor, v, visited);
+        AllVerticesIter iter(*this);
+        if (!StrongConnection) {
+            while (!iter.IsDone())
+            {
+                Vertex *x = &(*iter);
+                if (!iter.IsDone())
+                {
+                    if (visited[x->Number()] == false)
+                    {
+                        DFS_visitor(&visitor, &(*iter), visited);
+                    }
+                }
+                ++iter;
+            }
+        }
+
+//        std::cout << "\nNumber of vertices counted: " << visitor.GetNumber() << std::endl;
+    }
+
+    void DFS_visitor(CountingVisitor *visitor, Vertex *v, std::vector<bool> &visited)
+    {
+        visitor->Visit(*v);
+        visited[v->Number()] = true;
+//        std::cout << v->Number() << " ";
+        EmanEdgesIter emanIter(*this, v->Number());
+        while (!emanIter.IsDone())
+        {
+            ++emanIter;
+            if (!emanIter.IsDone())
+            {
+                Edge vEdge = *emanIter;
+                Vertex *u = vEdge.Mate(v);
+                if (visited[u->Number()] == false)
+                    DFS_visitor(visitor, u, visited);
+            }
+        }
+    }
+
+    void createAdjacencyMatrix(int n)
+    {
+        std::vector<Edge *> column(n, nullptr);
+        for (int i = 0; i < n; i++)
+        {
+            adjacencyMatrix.push_back(column);
+        }
+    }
+    GraphAsMatrix(int n, bool b)
+    {
+        isDirected = b;
+        numberOfVertices = n;
+        for (int i = 0; i < n; i++)
+        {
+            Vertex *v = new Vertex(i);
+            vertices.push_back(v);
+        }
+        createAdjacencyMatrix(n);
+    }
+
+    int NumberOfVertices()
+    {
+        return numberOfVertices;
+    }
+
+    bool IsDirected()
+    {
+        return isDirected;
+    }
+
+    int NumberOfEdges()
+    {
+        return numberOfEdges;
+    }
+
+    bool IsEdge(int u, int v)
+    {
+        bool ifEdgeExists = false;
+        if ((u > numberOfVertices) || (v > numberOfVertices) || (u == v))
+        {
+        }
+        else if (adjacencyMatrix[u][v] != NULL)
+        {
+            ifEdgeExists = true;
+        }
+        return ifEdgeExists;
+    }
+
+    void MakeNull()
+    {
+        for (int i = 0; i < numberOfVertices; i++)
+        {
+            for (int j = 0; j < numberOfVertices; j++)
+                adjacencyMatrix[i][j] = NULL;
+        }
+        numberOfEdges = 0;
+    }
+
+    void AddEdge(int u, int v)
+    {
+        if (!IsEdge(u, v) && u != v)
+        {
+            Edge *edge = new Edge(vertices[u], vertices[v]);
+            adjacencyMatrix[u][v] = edge;
+            numberOfEdges++;
+            if (!isDirected)
+            {
+                adjacencyMatrix[v][u] = edge;
+            }
+        }
+    }
+
+    Edge *SelectEdge(int u, int v)
+    {
+        if (IsEdge(u, v))
+            return adjacencyMatrix[u][v];
+    }
+
+    Vertex *SelectVertex(int v)
+    {
+        return vertices[v];
+    }
+
+    Iterator<Vertex> &VerticesIter()
+    {
+        return *new AllVerticesIter(*this);
+    }
+    Iterator<Edge> &EdgesIter()
+    {
+        return *new AllEdgesIter(*this);
+    }
+    Iterator<Edge> &EmanatingEdgesIter(int v)
+    {
+        return *new EmanEdgesIter(*this, v);
+    }
+    Iterator<Edge> &IncidentEdgesIter(int v)
+    {
+        return *new InciEdgesIter(*this, v);
+    }
+};
