d724ff00ab592e2713c9eb708e509da9c06e3c18
mawojcik <wojcik.maciej.k@gmail.com>
2023-06-10 23:10:15 +0200

    delete number assignment

--- Semestr_II/projekt_skarbonki/Vertex.h
+++ Semestr_II/projekt_skarbonki/Vertex.h
@@ -2,24 +2,14 @@
 
 class Vertex {
 private:
-    int number = -1;
+    int number;
 public:
     int weight;
-    bool visited = false;
     std::string label;
     Vertex(int n) {
         number = n;
     }
     int Number() const {
         return number;
-    };
-
-    friend bool operator==(const Vertex& v0, const Vertex& v1) {
-        return v0.number == v1.number;
-    }
-    friend std::ostream& operator<<(std::ostream& ost, Vertex const& v)
-    {
-        ost << v.number;
-        return ost;
     }
 };
